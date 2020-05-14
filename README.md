8987815c27ab4fa11c7cec3136a4c9b9d173f409
Martin Schnabel <ms@marmalade.group>
2020-05-14 17:10:39 +0200

    installer

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 - this library adds backward compatibility for `makaira/connect` module for Oxid versions older then 6.2
 - makes symfony dependency injection through `services.yaml` in module directories available
 - makes console commands callable through `./vendor/makaira/connect-oxid-compat/bin/console`
+- for oxid < 6.0 you may add this in your project compose.json to let composer copy the connect module to the modules directory
+  ```yaml
+    "scripts": {
+      "post-update-cmd": "Makaira\\ConnectCompat\\Composer::postUpdate",
+      "post-install-cmd": "Makaira\\ConnectCompat\\Composer::postUpdate"
+    },
+  ```
---
+++ src/Makaira/ConnectCompat/Composer.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Makaira\ConnectCompat;
+
+use Composer\Script\Event;
+
+class Composer
+{
+    public static function postUpdate(Event $event)
+    {
+        $packages = $event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages();
+        $installationManager = $event->getComposer()->getInstallationManager();
+
+        $foundOxid6 = false;
+        $connectDir = null;
+
+        foreach ($packages as $package) {
+            if ($package->getName() === 'oxid-esales/oxideshop-metapackage-ce') {
+                if (\version_compare($package->getVersion(), '6.0.0', '>=')) {
+                    $foundOxid6 = true;
+                }
+            }
+
+            if ($package->getName() === 'makaira/connect') {
+                $installationManager = $event->getComposer()->getInstallationManager();
+                $connectDir = $installationManager->getInstallPath($package);
+            }
+        }
+
+        if (!$foundOxid6) {
+            $root = __DIR__ . '/../../../../../../';
+            $modules = null;
+            foreach (['modules', 'web/modules', 'source/modules'] as $part) {
+                if (\is_dir($root . $part)) {
+                    $modules = $root . $part;
+                    break;
+                }
+            }
+
+            if (!$modules) {
+                echo "Modules directory not found.";
+                return;
+            }
+
+            $target = realpath($modules) . '/makaira/connect';
+            echo "Mirror $connectDir\nto     $target\n";
+
+            $fs = new \Symfony\Component\Filesystem\Filesystem();
+            $fs->mirror($connectDir, $target, null, [
+                'override' => true,
+                'delete' => true
+            ]);
+        }
+    }
+}
